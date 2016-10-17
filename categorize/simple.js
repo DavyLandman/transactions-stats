636f69eba8ade5fd42d3f23074324f960b419cae
Davy Landman <davy.landman@gmail.com>
2016-10-17 17:51:45 +0200

    More consistent indenting

--- categorize/simple.js
+++ categorize/simple.js
@@ -28,29 +28,27 @@ class CategoryMatcher {
 module.exports = { 
     patterns : [
         new CategoryMatcher("Pinautomaten", /^BEA\s*NR:/, [
-                [/[0-9] C EST BON ALMERE/, categories.ETEN_VERS],
-                [/[0-9] SLAGERIJ AHMED PAPA/, categories.ETEN_VERS],
-                [/[0-9] CCV\*V\.O\.F\. ROBIN EN KE/, categories.ETEN_VERS],
-                [/[0-9] ALBERT HEIJN [0-9]+ [A-Z]+/, categories.SUPERMARKT],
+            [/[0-9] C EST BON ALMERE/, categories.ETEN_VERS],
+            [/[0-9] SLAGERIJ AHMED PAPA/, categories.ETEN_VERS],
+            [/[0-9] CCV\*V\.O\.F\. ROBIN EN KE/, categories.ETEN_VERS],
+            [/[0-9] ALBERT HEIJN [0-9]+ [A-Z]+/, categories.SUPERMARKT],
 
-                [/BENZINESTA/, categories.TANKEN],
-                [/HOSPITAALGARAGE/, categories.PARKEREN],
+            [/BENZINESTA/, categories.TANKEN],
+            [/HOSPITAALGARAGE/, categories.PARKEREN],
 
-                [/[0-9] SPAR SCIENCEPARK AMSTERD/, categories.ETEN_WERK],
+            [/[0-9] SPAR SCIENCEPARK AMSTERD/, categories.ETEN_WERK],
 
-                [/[0-9] STRANDPAVILJOEN ALOH/, categories.UIT_ETEN],
-                [/[0-9] CHIANG MAI /, categories.UIT_ETEN],
-            ]
-        ),
+            [/[0-9] STRANDPAVILJOEN ALOH/, categories.UIT_ETEN],
+            [/[0-9] CHIANG MAI /, categories.UIT_ETEN],
+        ]),
         new CategoryMatcher("Incasso", /^\/TRTP\/SEPA INCASSO/, [
-                [/\/TLS BV INZ\. OV-CHIPKAART/, categories.OPENBAAR_VERVOER],
-                [/\/NS GROEP IZ NS REIZIGERS/, categories.OPENBAAR_VERVOER],
+            [/\/TLS BV INZ\. OV-CHIPKAART/, categories.OPENBAAR_VERVOER],
+            [/\/NS GROEP IZ NS REIZIGERS/, categories.OPENBAAR_VERVOER],
 
-                [/\/STICHTING BEWAARDER ACHM/, categories.HUUR_HUIS],
-                [/\/XS4ALL INTERNET B\.V\./, categories.INTERNET],
+            [/\/STICHTING BEWAARDER ACHM/, categories.HUUR_HUIS],
+            [/\/XS4ALL INTERNET B\.V\./, categories.INTERNET],
 
-                [/\/BROEKHUIS ASSURANTIE/, categories.AUTOVERZEKERING],
-            ]
-        )
+            [/\/BROEKHUIS ASSURANTIE/, categories.AUTOVERZEKERING],
+        ])
     ]
 };
